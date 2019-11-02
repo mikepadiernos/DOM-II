e4098e8e3c7d0b4fd884ad0c862db3726a455b1c
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-01 21:48:03 -0400

    feature: added preventDefault code

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
 // Your code goes here
+
+
+
+
+
+
+
+const noRefresh = document.querySelectorAll('.nav-link');
+noRefresh.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+    });
+});
