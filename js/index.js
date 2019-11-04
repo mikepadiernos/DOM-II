3b3b9ead0ce128377cac304dba8e3911ab38c702
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-04 12:40:12 -0500

    ADDITION: added events drag, dragend, & stopPropagation

--- js/index.js
+++ js/index.js
@@ -1,11 +1,19 @@
 // Your code goes here
 
-// EVENT: resize
-const fullScreen = document.querySelector('.nav-container');
-function navWidth() {
-    fullScreen.style.maxWidth = '90%';
-}
-window.onresize = navWidth;
+// EVENT: copy
+const copy = document.querySelector('.intro h2');
+copy.addEventListener('copy', () => {
+    copy.style.textTransform = 'uppercase';
+});
+
+// EVENT: drag & dragend
+const drag = document.querySelector('.intro');
+drag.addEventListener('drag', () => {
+    drag.style.background = 'pink';
+});
+drag.addEventListener('dragend', () => {
+    drag.style.background = 'white';
+});
 
 // EVENT: click & dblclick
 const destination = document.querySelector('.content-destination');
@@ -32,7 +40,7 @@ const btnHover = document.querySelectorAll('div.btn');
 let btnGroup = [...btnHover];
 btnGroup.forEach(btn => {
     btn.addEventListener('wheel', (event) => {
-        event.target.style.borderRadius = '3px';
+        event.target.style.borderRadius = '4px';
     });
 });
 
@@ -40,6 +48,23 @@ btnGroup.forEach(btn => {
 const reset = document.querySelector('.footer p');
 reset.addEventListener('contextmenu', () => {
     reset.style.fontSize = '1.8rem';
+});
+
+// EVENT: resize
+const fullScreen = document.querySelector('.nav-container');
+window.onresize = () => fullScreen.style.maxWidth = '90%';
+
+// EXECUTE: stopPropagation
+let mainNav = document.querySelector('.main-navigation');
+mainNav.addEventListener('click', () => {
+    mainNav.style.background = 'lime';
+});
+let navDiv = document.querySelector('nav.nav');
+// navDiv.addEventListener('click', () => {
+//     navDiv.style.visibility = 'hidden';
+navDiv.addEventListener('click', (event) => {
+    navDiv.style.visibility = 'hidden';
+    event.stopPropagation();
 });
 
 // EXECUTE: preventDefault
