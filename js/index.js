6afa6c46a671aca9f76f26eb1f9e8bf8d33e6de4
Michael Padiernos <mike.padiernos@gmail.com>
2019-11-04 11:41:21 -0500

    ADDITION: worked on events: wheel & resize

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
 // Your code goes here
 
+// EVENT: resize
 const fullScreen = document.querySelector('.nav-container');
-window.addEventListener('resize', () => {
-     fullScreen.style.maxWidth = '90%';
-});
+function navWidth() {
+    fullScreen.style.maxWidth = '90%';
+}
+window.onresize = navWidth;
 
-
-
-
+// EVENT: click & dblclick
 const destination = document.querySelector('.content-destination');
 destination.addEventListener('click', () => {
     destination.style.width = '100%';
@@ -16,10 +16,10 @@ destination.addEventListener('dblclick', () => {
     destination.style.width = '75%';
 });
 
+// EVENT: mouseenter & mouseleave
 const destImage = document.querySelector('.content-destination img');
 destImage.addEventListener('mouseenter', () => {
     destImage.style.width = '100%';
-    // destImage.style.borderRadius = '4px';
     destImage.style.transform = 'scale(1.2)';
     destImage.style.transition = 'all 0.3s';
 });
@@ -27,17 +27,22 @@ destImage.addEventListener('mouseleave', () => {
     destImage.style.transform = 'scale(1)';
 });
 
-const btnHover = document.querySelector('.btn');
-btnHover.addEventListener('wheel', () => {
-        btnHover.style.borderRadius = '3px';
+// EVENT: wheel
+const btnHover = document.querySelectorAll('div.btn');
+let btnGroup = [...btnHover];
+btnGroup.forEach(btn => {
+    btn.addEventListener('wheel', (event) => {
+        event.target.style.borderRadius = '3px';
     });
-console.log(btnHover);
+});
 
+// EVENT: contextmenu
 const reset = document.querySelector('.footer p');
 reset.addEventListener('contextmenu', () => {
     reset.style.fontSize = '1.8rem';
 });
 
+// EXECUTE: preventDefault
 const noRefresh = document.querySelectorAll('.nav-link');
 noRefresh.forEach(link => {
     link.addEventListener('click', (event) => {
